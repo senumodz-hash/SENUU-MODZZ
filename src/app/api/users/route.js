99091550932e5189886a5c92eff50acbe19986d1
senumodz-hash <senumodz@gmail.com>
2026-01-13 15:36:58 +0530

    Update route.js

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,11 +1,18 @@
 import { connectDB } from "../../../lib/db";
-import User from "@/models/User";
-import bcrypt from "bcrypt";
+import User from "../../../models/User";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const { name, email, password } = await req.json();
-  await connectDB();
-  const hash = await bcrypt.hash(password, 10);
-  await User.create({ name, email, password: hash });
-  return Response.json({ success: true });
+  try {
+    const { name, email, password } = await req.json();
+    await connectDB();
+
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hash });
+
+    return NextResponse.json({ success: true, user: { id: user._id, email: user.email } });
+  } catch (err) {
+    return NextResponse.json({ error: "Failed to register user" }, { status: 500 });
+  }
 }
