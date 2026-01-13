048860c36cefa50955c1cfcfe1847579738d7d82
senumodz-hash <senumodz@gmail.com>
2026-01-13 15:10:07 +0530

    Create WhatsAppFloat.js

---
+++ src/components/WhatsAppFloat.js
@@ -0,0 +1,11 @@
+export default function WhatsAppFloat() {
+  return (
+    <a
+      href="https://wa.me/94743903048"
+      target="_blank"
+      className="fixed bottom-6 right-6 bg-green-500 text-black px-5 py-3 rounded-full font-bold"
+    >
+      WhatsApp
+    </a>
+  );
+}
