48c650409b9a893a343e8df4b12d0a0178503b1e
senumodz-hash <senumodz@gmail.com>
2026-01-13 15:08:19 +0530

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Pricing from "@/components/Pricing";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-center mt-10 text-green-400">
+        Senu Modz
+      </h1>
+      <p className="text-center text-gray-400 mt-4">
+        WhatsApp Bot Panels & Automation
+      </p>
+      <Pricing />
+    </>
+  );
+}
